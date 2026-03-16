8c74c3f14079f2fd0bfc7b6e969bc4bb38f56ca1
jnancoo1 <92116647+jnancoo1@users.noreply.github.com>
2026-03-15 22:28:58 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -10,31 +10,32 @@ from cocotb.triggers import ClockCycles
 async def test_project(dut):
     dut._log.info("Start")
 
-    # Set the clock period to 10 us (100 KHz)
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
 
     # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 5)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Full adder truth table tests
+    for i in range(8):
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+        dut.ui_in.value = i
+        await ClockCycles(dut.clk, 1)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+        A = (i >> 0) & 1
+        B = (i >> 1) & 1
+        C = (i >> 2) & 1
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+        SUM = A ^ B ^ C
+        CARRY = (A & B) | ((A ^ B) & C)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+        expected = (SUM << 1) | CARRY
+
+        dut._log.info(f"A={A} B={B} C={C} expected={expected}")
+
+        assert dut.uo_out.value & 0b11 == expected
