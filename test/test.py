47aff3b45e4fac0fb403503651549332dd204386
jnancoo1 <92116647+jnancoo1@users.noreply.github.com>
2026-03-15 22:34:18 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,41 +1,17 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
+for i in range(8):
 
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+    dut.ui_in.value = i
+    await ClockCycles(dut.clk, 1)
 
+    A = (i >> 0) & 1
+    B = (i >> 1) & 1
+    C = (i >> 2) & 1
 
-@cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+    SUM = A ^ B ^ C
+    CARRY = (A & B) | ((A ^ B) & C)
 
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+    expected = (SUM << 1) | CARRY
 
-    # Reset
-    dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
-    dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 5)
-    dut.rst_n.value = 1
+    dut._log.info(f"A={A} B={B} C={C} expected={expected}")
 
-    # Full adder truth table tests
-    for i in range(8):
-
-        dut.ui_in.value = i
-        await ClockCycles(dut.clk, 1)
-
-        A = (i >> 0) & 1
-        B = (i >> 1) & 1
-        C = (i >> 2) & 1
-
-        SUM = A ^ B ^ C
-        CARRY = (A & B) | ((A ^ B) & C)
-
-        expected = (SUM << 1) | CARRY
-
-        dut._log.info(f"A={A} B={B} C={C} expected={expected}")
-
-        assert dut.uo_out.value & 0b11 == expected
+    assert (dut.uo_out.value.integer & 0b11) == expected
