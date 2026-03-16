449b76f54ba23786b9fa11ac10649e4a6f3aba73
jnancoo1 <92116647+jnancoo1@users.noreply.github.com>
2026-03-15 22:35:42 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,3 +1,27 @@
+# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-License-Identifier: Apache-2.0
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import ClockCycles
+
+
+@cocotb.test()
+async def test_project(dut):
+    dut._log.info("Start")
+
+    clock = Clock(dut.clk, 10, unit="us")
+    cocotb.start_soon(clock.start())
+
+    # Reset
+    dut.ena.value = 1
+    dut.ui_in.value = 0
+    dut.uio_in.value = 0
+    dut.rst_n.value = 0
+    await ClockCycles(dut.clk, 5)
+    dut.rst_n.value = 1
+
+    # Full adder truth table tests
 for i in range(8):
 
     dut.ui_in.value = i
